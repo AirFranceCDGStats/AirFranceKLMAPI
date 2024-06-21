5b0718e38d6813bf36be414baa6af75e99266f5a
PaulBayfield <137620008+PaulBayfield@users.noreply.github.com>
2024-06-21 22:07:08 +0200

    Ajout d'une route pour avoir tout l'historique sur un mois

--- models/status.py
+++ models/status.py
@@ -26,3 +26,62 @@ class StatusResponse:
         description="Message de retour",
         example="L'API est en ligne."
     )
+
+
+class HistoriqueStatsResponse:
+    airports = openapi.Integer(
+        description="Nombre d'aéroports",
+        example=200,
+    )
+    flights = openapi.Integer(
+        description="Nombre de vols",
+        example=80_000,
+    )
+    companies = openapi.Integer(
+        description="Nombre de compagnies",
+        example=50,
+    )
+    steps = openapi.Integer(
+        description="Nombre d'étapes",
+        example=200,
+    )
+    on_time = openapi.Integer(
+        description="Nombre d'avions à l'heure",
+        example=200,
+    )
+    delayed_departure = openapi.Integer(
+        description="Nombre de départ retardé",
+        example=200,
+    )
+    delayed_arrival = openapi.Integer(
+        description="Nombre d'arrivée retardée",
+        example=200,
+    )
+    delayed = openapi.Integer(
+        description="Nombre de vols retardés (départ et arrivée)",
+        example=200,
+    )
+    cancelled = openapi.Integer(
+        description="Nombre de vols annulés",
+        example=200,
+    )
+    gaz = openapi.Integer(
+        description="Nombre de litres de gaz consommés",
+        example=200,
+    )
+
+
+class HistoriqueResponse:
+    success = openapi.Boolean(
+        description="Statut de la requête",
+        example=True,
+    )
+    code = openapi.Integer(
+        description="Code de retour",
+        example=200,
+    )
+    info =  openapi.Array(
+        items=HistoriqueStatsResponse,
+        description="Historique de l'API",
+    )
+    
--- routes/service/service.py
+++ routes/service/service.py
@@ -1,4 +1,4 @@
-from models.status import StatusResponse
+from models.status import StatusResponse, HistoriqueResponse
 from models.responses import RateLimitedResponse
 from sanic.response import JSONResponse, json
 from sanic import Blueprint, Request
@@ -49,6 +49,65 @@ async def getStatus(request: Request) -> JSONResponse:
                 "companies": len(request.app.ctx.cache.companies),
             },
             "message": "L'API est en ligne."
+        },
+        status=200
+    )
+
+
+# /historique
+@bp.route("/historique", methods=["GET"])
+@openapi.definition(
+    summary="Historique de l'API",
+    description="Retourne l'historique de l'API.",
+    tag="Service",
+)
+@openapi.response(
+    status=200,
+    content={
+        "application/json": HistoriqueResponse
+    },
+    description="L'API est en ligne."
+)
+@openapi.response(
+    status=429,
+    content={
+        "application/json": RateLimitedResponse
+    },
+    description="Vous avez envoyé trop de requêtes. Veuillez réessayer plus tard."
+)
+async def getHistorique(request: Request) -> JSONResponse:
+    """
+    Retourne l'historique de l'API.
+
+    :return: JSONResponse
+    """
+    data = []
+
+    async with request.app.ctx.pool.acquire() as connection:
+        historique = await connection.fetch("SELECT * FROM HISTORIQUE ORDER BY DATE DESC LIMIT 30")
+
+    for row in historique:
+        data.append(
+            {
+                "date": row["date"],
+                "airports": row["nbaeroports"],
+                "flights": row["nbvols"],
+                "companies": row["nbavions"],
+                "steps": row["nbetapes"],
+                "on_time": row["nbetape_on_time"],
+                "delayed_departure": row["nbetape_delayed_departure"],
+                "delayed_arrival": row["nbetape_delayed_arrival"],
+                "delayed": row["nbetape_delayed"],
+                "cancelled": row["nbetape_cancelled"],
+                "gaz": row["gaz"],
+            }
+        )
+
+    return json(
+        {
+            "success": True,
+            "code": 200,
+            "info": data
         },
         status=200
     )
